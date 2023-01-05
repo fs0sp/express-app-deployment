468f9f6136c12cb877cd91f14eadd21e4ec71134
Shivam <shivampokhriyal@rocketmail.com>
2023-01-05 23:08:46 +0530

    initial

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const data = require('./temp');
+const port = process.env.PORT;
+
+app.get('/', (req, res)=> {
+  res.send('GO TO /data');
+})
+
+app.get('/data', (req, res) => {
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log("...", port);
+});
---
+++ temp.js
@@ -0,0 +1,147 @@
+const data = [
+  {
+    "email": "melany.wijngaard@example.com",
+    "gender": "female",
+    "phone_number": "(727)-033-9347",
+    "birthdate": 608022796,
+    "location": {
+      "street": "2431 predikherenkerkhof",
+      "city": "staphorst",
+      "state": "gelderland",
+      "postcode": 64265
+    },
+    "username": "bigpeacock217",
+    "password": "eagle",
+    "first_name": "melany",
+    "last_name": "wijngaard",
+    "title": "miss",
+    "picture": "women/70.jpg"
+  },
+  {
+    "email": "nanna.pedersen@example.com",
+    "gender": "female",
+    "phone_number": "43672992",
+    "birthdate": 591428535,
+    "location": {
+      "street": "2177 fåborgvej",
+      "city": "aarhus",
+      "state": "syddanmark",
+      "postcode": 87547
+    },
+    "username": "purpleduck599",
+    "password": "davids",
+    "first_name": "nanna",
+    "last_name": "pedersen",
+    "title": "ms",
+    "picture": "women/68.jpg"
+  },
+  {
+    "email": "amelia.mercier@example.com",
+    "gender": "female",
+    "phone_number": "(168)-747-5950",
+    "birthdate": 1132298571,
+    "location": {
+      "street": "7454 rue duquesne",
+      "city": "echandens-denges",
+      "state": "vaud",
+      "postcode": 3811
+    },
+    "username": "whitefrog218",
+    "password": "forest",
+    "first_name": "amelia",
+    "last_name": "mercier",
+    "title": "madame",
+    "picture": "women/91.jpg"
+  },
+  {
+    "email": "sarah.oliver@example.com",
+    "gender": "female",
+    "phone_number": "0761-814-654",
+    "birthdate": 1038915780,
+    "location": {
+      "street": "3503 manor road",
+      "city": "manchester",
+      "state": "highlands and islands",
+      "postcode": "I30 5ZF"
+    },
+    "username": "purplebear893",
+    "password": "boat",
+    "first_name": "sarah",
+    "last_name": "oliver",
+    "title": "ms",
+    "picture": "women/73.jpg"
+  },
+  {
+    "email": "عسل.مرادی@example.com",
+    "gender": "female",
+    "phone_number": "0925-326-2063",
+    "birthdate": 417058382,
+    "location": {
+      "street": "4981 شهید بهشتی",
+      "city": "پاکدشت",
+      "state": "مرکزی",
+      "postcode": 68555
+    },
+    "username": "ticklishfish813",
+    "password": "zaq12wsx",
+    "first_name": "عسل",
+    "last_name": "مرادی",
+    "title": "mrs",
+    "picture": "women/65.jpg"
+  },
+  {
+    "email": "özkan.tekelioğlu@example.com",
+    "gender": "male",
+    "phone_number": "(074)-376-7384",
+    "birthdate": 616226885,
+    "location": {
+      "street": "2260 anafartalar cd",
+      "city": "afyonkarahisar",
+      "state": "amasya",
+      "postcode": 31532
+    },
+    "username": "organiccat298",
+    "password": "coventry",
+    "first_name": "özkan",
+    "last_name": "tekelioğlu",
+    "title": "mr",
+    "picture": "men/69.jpg"
+  },
+  {
+    "email": "angela.newman@example.com",
+    "gender": "female",
+    "phone_number": "081-718-3989",
+    "birthdate": 479824115,
+    "location": {
+      "street": "3159 west street",
+      "city": "celbridge",
+      "state": "colorado",
+      "postcode": 82776
+    },
+    "username": "smallgorilla567",
+    "password": "1234567",
+    "first_name": "angela",
+    "last_name": "newman",
+    "title": "mrs",
+    "picture": "women/82.jpg"
+  },
+  {
+    "email": "buse.dağdaş@example.com",
+    "gender": "female",
+    "phone_number": "(855)-841-4810",
+    "birthdate": 1316736697,
+    "location": {
+      "street": "6428 anafartalar cd",
+      "city": "malatya",
+      "state": "mardin",
+      "postcode": 28227
+    },
+    "username": "crazypeacock394",
+    "password": "paulie",
+    "first_name": "buse",
+    "last_name": "dağdaş",
+    "title": "ms",
+    "picture": "women/14.jpg"
+  },];
+
+module.exports = data;
